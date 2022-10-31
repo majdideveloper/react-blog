217cafaf50e67e453c8e3bd589638f51ecdae392
majdi aribi <majdi.developer@gmail.com>
2022-10-31 16:16:46 +0100

    create sidebar and posts components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import './home.css'
 import Header from '../../header/Header'
+import Posts from '../../posts/Posts'
+import Sidebar from '../../sidebar/Sidebar'
 
 
 export default function Home() {
   return (
-    <div className='home'>
-        <Header />
-        homepage
+    <>
+      <Header />
+      <div className='home'>
+        <Posts/>
+        <Sidebar/>
 
-    </div>
+
+
+      </div>
+    </>
   )
 }
